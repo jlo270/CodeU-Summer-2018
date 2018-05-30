623743cf2f8048a2764468668dba536c19ecd714
jlo270 <12497700+jlo270@users.noreply.github.com>
2018-05-29 23:31:28 -0500

    Corrected path for adminpage.jsp, Created early AdminServletTest and modified AdminServlet prototype

--- src/main/java/codeu/controller/AdminServlet.java
+++ src/main/java/codeu/controller/AdminServlet.java
@@ -1,5 +1,23 @@
 package codeu.controller;
 
-public class AdminServlet {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class AdminServlet extends HttpServlet {
+	
+	/**
+	 * This function fires when a user requests the /admin URL. It forwards the request to adminpage.jsp
+	 */
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+		throws IOException, ServletException {
+		request.getRequestDispatcher("/WEB-INF/view/adminpage.jsp").forward(request, response);
+	}
+	
+	
 }
