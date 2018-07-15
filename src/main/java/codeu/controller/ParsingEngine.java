ef21e33f1bfa5b01841d08e3a00e298ccd193010
Jagdish Ghimire <jagdish.ghimire1@gmail.com>
2018-07-15 16:55:48 -0500

    Resolved previous errors.

--- src/main/java/codeu/controller/ParsingEngine.java
+++ src/main/java/codeu/controller/ParsingEngine.java
@@ -2,26 +2,32 @@ package codeu.controller;
 
 import codeu.model.data.BotRequest;
 import codeu.model.data.BotStructure;
+import codeu.model.data.Message;
 import codeu.model.data.RoutingEngine;
 import java.util.regex.*;
 
 
 public class parsingEngine {
- 	public void ParseCommands(String message){
+ 	public void ParseCommands(Message message){
  		BotRequest request;
 
  		// ^ to make sure we always start at the beginning of a string without whitespace.
  		// / to detect our command syntax.
+ 		// // to escape the following /.  
  		// (?P<command_name>\w+) as a required parameter capture group that is made up of at least 1 alphanumeric character
  		// \s as a white space chracter. : space, tab, newline, carriage return, vertical tab. 
- 		final String commandRegex = "^/(?P<command_name>\w+)(?:\s+(?P<argument>\w+))?\s*$";
-
- 		RegexMatch match = regex.match(commandRegex, message);
-
-		if (match != null) {
+ 		final String commandRegex = "^/(?P<command_name>\\w+)(?:\\s+(?P<argument>\\w+))?\\s*$";
+ 		// compile the Regex command
+ 		Pattern pattern = Pattern.compile(commandRegex);
+ 		
+ 		//matches the regex command with the Message. 
+		Matcher match = pattern.matcher(message.getContent());
+		
+		//if match is found, pass the command to RoutingEngine. 
+		if (match.find()) {
 			
-			request.command = match.get(command_name);
-			request.arguments.argument = match.get(argument); 
+			request.command = match.group("command_name");
+			request.arguments.argument = match.group("argument"); 
 			request.conversationId = message.getConversationId();
 
 			RoutingEngine.routeCommand(request); 
