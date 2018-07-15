3e731ca57282970d52a58a724167daab243a8851
Hunter J <hajames@crimson.ua.edu>
2018-07-15 18:26:39 -0500

    Rebuilding using `getConversationWithId` from activity feed PR.

--- src/main/java/codeu/controller/RocketBotHandler.java
+++ src/main/java/codeu/controller/RocketBotHandler.java
@@ -29,7 +29,6 @@ public class RocketBotHandler implements BotHandlerInterface {
                        BOT_NAME,
           "$2a$10$/zf4WlT2Z6tB5sULB9Wec.QQdawmF0f1SbqBw5EeJg5uoVpKFFXAa",
                        Instant.now());
-    //currentBot.setRequest(request);
 
     // Find conversation where command was sent/bot was triggered
     UUID conversationId = request.getConversationId();
