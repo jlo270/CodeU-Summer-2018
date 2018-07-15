a4e69684ddb0dcc4356b9b46e2aae0acfc19e425
Jagdish Ghimire <jagdish.ghimire1@gmail.com>
2018-07-15 17:15:21 -0500

    Resolved the Previous issues.

--- src/main/java/codeu/controller/ParsingEngine.java
+++ src/main/java/codeu/controller/ParsingEngine.java
@@ -4,18 +4,15 @@ import codeu.model.data.BotRequest;
 import codeu.model.data.BotStructure;
 import codeu.model.data.Message;
 import codeu.model.data.RoutingEngine;
-import java.util.regex.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 
 public class parsingEngine {
  	public void ParseCommands(Message message){
- 		BotRequest request;
-
- 		// ^ to make sure we always start at the beginning of a string without whitespace.
- 		// / to detect our command syntax.
- 		// // to escape the following /.  
- 		// (?P<command_name>\w+) as a required parameter capture group that is made up of at least 1 alphanumeric character
- 		// \s as a white space chracter. : space, tab, newline, carriage return, vertical tab. 
+ 		
+ 
  		final String commandRegex = "^/(?P<command_name>\\w+)(?:\\s+(?P<argument>\\w+))?\\s*$";
  		// compile the Regex command
  		Pattern pattern = Pattern.compile(commandRegex);
@@ -25,9 +22,9 @@ public class parsingEngine {
 		
 		//if match is found, pass the command to RoutingEngine. 
 		if (match.find()) {
-			
+			BotRequest request;
 			request.command = match.group("command_name");
-			request.arguments.argument = match.group("argument"); 
+			//request.arguments.argument = match.group("argument"); 
 			request.conversationId = message.getConversationId();
 
 			RoutingEngine.routeCommand(request); 
