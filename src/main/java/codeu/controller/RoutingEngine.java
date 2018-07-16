f92fc0f5a03dd4169bd7ebec0f9c3d62d0d2983d
jlo270 <12497700+jlo270@users.noreply.github.com>
2018-07-15 19:40:52 -0500

    Modified routeCommand, build currently broken

--- src/main/java/codeu/controller/RoutingEngine.java
+++ src/main/java/codeu/controller/RoutingEngine.java
@@ -1,23 +1,25 @@
 package codeu.controller;
 
 import codeu.model.data.BotRequest;
+import java.lang.IllegalArgumentException;
 
 /** This is the Routing Engine that will receive a BotRequest and pass it to the 
  * appropriate BotHandler class*/
 public class RoutingEngine {
 
-  
+  private BotHandlerInterface botHandler;
   /**
    * primary routing function that passes the request to the appropriate Handler
    * needs to be updated for each bot
    */
-  public String routeCommand(BotRequest request) {
+  public void routeCommand(BotRequest request) {
 
     switch (request.getCommand()) {
+      //case = "rocket":
+        //botHandler = new RocketBotHandler();
       default:
-        return "error, command not found";
-        
+        throw new IllegalArgumentException();
     }
-
+    botHandler.handler(request);
   }
 }
