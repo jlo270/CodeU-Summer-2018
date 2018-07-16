62af4f679db0e05656b920cd9eb592de2540b44c
Jagdish Ghimire <jagdish.ghimire1@gmail.com>
2018-07-15 20:21:50 -0500

    Resolved some issues.

--- src/main/java/codeu/controller/ParsingEngine.java
+++ src/main/java/codeu/controller/ParsingEngine.java
@@ -10,34 +10,36 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
-	
+/*
+ * This class parses the BotCommands and will be invoked from ChatServlet.
+ * MessangeContent of DoPost() in ChatServlet will be passed in as parameter.  
+ * 
+ */
+public class ParsingEngine {
+  /*
+   * This regex looks for a command name as /<command_name>, where command_name is alphanumeric, followed by whitespace-separated 0-N alphanumeric arguments.
+   * Examples:
+   * /command
+   * /command arg1
+   * /command arg1 arg2 arg3 arg4
+   */
 
-public class parsingEngine {
-	/* 
-	 * Here, regex is a better way to parse bot commands rather than matching them with some keywords.
-	 * Regex can parse varying bot request according to pattern. 
-	 * For example, the following regex can parse "/time", "/  time" or "/time    " as "time". 
-	 * 
-	 */
-	
-	static final String commandRegex = "^/(?P<command_name>\\w+)(?:\\s+(?P<argument>\\w+))?\\s*$";
-	
-	// compile the Regex command
-	 static final Pattern pattern = Pattern.compile(commandRegex);
-	
- 	public void ParseCommands(Message message){ 
- 		final UUID conversationId;
- 		
- 		
- 		//matches the regex command with the Message. 
-		Matcher match = pattern.matcher(message.getContent());
-		
-		//if match is found, pass the command to RoutingEngine. 
-		if (match.find()) {
-			final BotRequest request = new BotRequest(match.group("command_name"), match.group("argument"), conversationId ); 
-			request.conversationId = message.getConversationId();
+  static final String commandRegex = "^/(?P<command_name>\\w+)(?:\\s+(?P<argument>\\w+))?\\s*$";
 
-			RoutingEngine.routeCommand(request); 
-		}
-	}
+  // compile the Regex command
+  static final Pattern pattern = Pattern.compile(commandRegex);
+
+  public static void parseCommands(Message message) {
+    
+    // Matches the regex command with the Message.
+    final Matcher match = pattern.matcher(message.getContent());
+    
+    // If match is found, pass the command to RoutingEngine.
+    if (match.find()) {
+      final BotRequest request = new BotRequest(match.group("command_name"), match.group("argument"),
+    		  message.getConversationId());;
+
+      RoutingEngine.routeCommand(request);
+    }
+  }
 }
