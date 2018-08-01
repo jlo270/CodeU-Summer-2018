f30abeab7b589047e5991008ddfb9432c01238e5
Hunter J <hajames@crimson.ua.edu>
2018-07-31 22:27:21 -0500

    Added unit test to test if a message is added to MessageStore by the bot handler.

--- src/main/java/codeu/controller/RocketBotHandler.java
+++ src/main/java/codeu/controller/RocketBotHandler.java
@@ -18,6 +18,8 @@ public class RocketBotHandler implements BotHandlerInterface {
 
   static final String BOT_NAME = "RocketBot";
   static final String BOT_OUTPUT = "3...2...1...BLASTOFF!";
+  static UUID messageId = UUID.randomUUID();
+  static Instant messageTime = Instant.now();
 
   /** Handles bot requests and sends as messages to conversations. */
   public void handler(BotRequest request) {
@@ -26,14 +28,24 @@ public class RocketBotHandler implements BotHandlerInterface {
 
     // Convert output and required arguments into Message
     Message message = new Message(
-        UUID.randomUUID(),
+        messageId,
         request.getConversationId(),
         BOT_USER_ID,
         BOT_OUTPUT,
-        Instant.now());
+        messageTime);
 
     // Send message to conversation
     messageStore.addMessage(message);
+  }
+
+  /** Accessor class that allows test UUIDs to match, even when randomly assigned. */
+  UUID getBotMessageId() {
+    return messageId;
+  }
+
+  /** Accessor class that allows test Instant to match current Instant. */
+  Instant getBotMessageTime() {
+    return messageTime;
   }
 
   /** Store class that gives access to Messages. */
--- src/test/java/codeu/controller/RocketBotHandlerTest.java
+++ src/test/java/codeu/controller/RocketBotHandlerTest.java
@@ -1,6 +1,7 @@
 package codeu.controller;
 
 import codeu.model.data.BotRequest;
+import codeu.model.data.Message;
 import codeu.model.data.User;
 import codeu.model.store.basic.UserStore;
 import codeu.model.store.basic.MessageStore;
@@ -21,6 +22,8 @@ public class RocketBotHandlerTest {
   private final UUID CONVERSATION_ID_ONE = UUID.randomUUID();
   private final UUID USER_ID_ONE = UUID.randomUUID();
   private RocketBotHandler rocketBot;
+  private final String OUTPUT_ONE = "3...2...1...BLASTOFF!";
+  private final UUID MESSAGE_ID_ONE = UUID.randomUUID();
 
   @Before
   public void setup() {
@@ -68,5 +71,41 @@ public class RocketBotHandlerTest {
     Assert.assertNull(mockUserStore.getUser("RocketBot"));
     rocketBot.handler(request);
     Assert.assertNotNull(mockUserStore.getUser("RocketBot"));
+  }
+
+  @Test
+  public void testMessageSent() {
+    User existingUser = new User(
+        USER_ID_ONE,
+        "RocketBot",
+        "unusable-password-hash",
+        Instant.ofEpochMilli(1000));
+    final List<User> userList = new ArrayList<>();
+    userList.add(existingUser);
+    mockUserStore.setUsers(userList);
+    ArrayList<String> args = new ArrayList<>();
+    BotRequest request = new BotRequest("rocket",args,CONVERSATION_ID_ONE);
+
+    Message testMessage = new Message(
+        rocketBot.getBotMessageId(),
+        request.getConversationId(),
+        USER_ID_ONE,
+        OUTPUT_ONE,
+        rocketBot.getBotMessageTime());
+
+    rocketBot.handler(request);
+
+    List<Message> messagesInConversation = mockMessageStore.getMessagesInConversation(request.getConversationId());
+    int numMessages = mockMessageStore.getNumMessages();
+    System.out.print("numMessages = ");
+    System.out.println(numMessages);
+
+    Assert.assertEquals(messagesInConversation.get(0).getAuthorId(),USER_ID_ONE);
+    Assert.assertEquals(messagesInConversation.get(0).getCreationTime(),rocketBot.getBotMessageTime());
+    Assert.assertEquals(messagesInConversation.get(0).getContent(),OUTPUT_ONE);
+    Assert.assertEquals(messagesInConversation.get(0).getId(),rocketBot.getBotMessageId());
+    Assert.assertEquals(messagesInConversation.get(0).getConversationId(),request.getConversationId());
+
+    //Assert.assertEquals(messagesInConversation.contains(testMessage), true);
   }
 }
