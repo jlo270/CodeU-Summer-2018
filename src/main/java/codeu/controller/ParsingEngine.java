f814daf6ff5afaed2e804cc2a73e9d9e35218ef7
Jagdish Ghimire <jagdish.ghimire1@gmail.com>
2018-08-01 11:37:30 -0500

    Added method to split the arguments to return as Arraylist.

--- src/main/java/codeu/controller/ParsingEngine.java
+++ src/main/java/codeu/controller/ParsingEngine.java
@@ -5,10 +5,11 @@ import codeu.model.data.BotStructure;
 import codeu.model.data.Message;
 import codeu.model.data.RoutingEngine;
 
+import java.util.ArrayList;
 import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+import java.util.Arrays;
 
 /*
  * This class parses the BotCommands and will be invoked from ChatServlet.
@@ -22,9 +23,13 @@ public class ParsingEngine {
    * /command
    * /command arg1
    * /command arg1 arg2 arg3 arg4
+   * Also this can handle any whitespace in between or trailing the arguments. 
+   * Examples:
+   * /command      args1
+   * /command   args1     args2
    */
 
-  static final String commandRegex = "^/(?<commandName>\\w+)(\\s+(?<arguments>\\w+))*$";
+  static final String commandRegex = "^/(?<commandName>\\w+)(\\s+(?<arguments>\\w+)?)*$";
 
   // compile the Regex command
   static final Pattern pattern = Pattern.compile(commandRegex);
@@ -35,11 +40,18 @@ public class ParsingEngine {
     final Matcher match = pattern.matcher(message.getContent());
     
     // If match is found, pass the command to RoutingEngine.
-    if (match.find()) {
-      final BotRequest request = new BotRequest(match.group("commandName"), match.group("arguments"),
-    		  message.getConversationId());;
+    if (match.matches()) {
+      final BotRequest request = new BotRequest(match.group("commandName"), splitArguments(match.group("arguments")),
+    		  message.getConversationId());
 
       RoutingEngine.routeCommand(request);
     }
+  }
+  
+  public static ArrayList<String> splitArguments(String arguments){
+	  String[] splitStr = arguments.trim().split("\\s+");
+	  ArrayList<String> arrayList = new ArrayList<String>(Arrays.asList(splitStr));
+	  return arrayList;
+
   }
 }
